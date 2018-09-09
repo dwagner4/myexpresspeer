5e0fea3e462bb37a474d2d65fd4375fee6cae9b7
Dean Wagner <dean@dfwagners.net>
2018-09-09 19:05:12 -0400

    working text RTC

---
+++ public/js/textDuplex.js
@@ -0,0 +1,54 @@
+"use strict";
+// https://medium.com/@rsylvian/webrtc-peer-to-peer-data-video-and-audio-calls-with-peer-js-24f79a09f67d
+
+
+// Define peer connections, streams and video elements.
+let peer1 = null;
+let peer1id = null;
+let remoteID = null;
+let conn = null;
+let stunIP = null;
+
+
+function doStart () {
+	// peer1 = new Peer();
+	stunIP = document.getElementById('stunip').value || "/";
+	peer1 = new Peer({host: stunIP, port: 9000, path: "peerjs", debug: 3});
+	peer1.on('open', function(id) {
+	  peer1id = id;
+	  console.log('I\'m peer1id and my id is: ' + peer1id);
+		document.getElementById('localid').innerHTML = peer1id;
+	});
+	peer1.on('connection', function(connc) {
+
+		conn = connc;
+		conn.on('open', function() {
+			console.log("connection is open after recving offer");
+	  // Receive messages
+		  conn.on('data', function(data) {
+		    console.log('Received', data);
+		  });
+		});
+	});
+	peer1.on('error', function (error) {
+		console.log(error);
+	});
+};
+
+// call peer2
+function doConnect () {
+	let peer2id = document.getElementById('remoteid').value;
+	conn = peer1.connect(peer2id);
+	conn.on('open', () => {
+		console.log("connection is open from this peer")
+	  // Receive messages
+	  conn.on('data', function(data) {
+	    console.log('Received', data);
+	  });
+		conn.send('Hello!');
+	});
+}
+
+function doSend () {
+	conn.send('Hello! again');
+}
